121fd1bb23a3c81ecfee3fffef190126e24e48db
aljagne <ajagne01@gmail.com>
2022-04-30 12:10:05 +0000

    NavLink with CN✅

--- src/Common/Nav.js
+++ src/Common/Nav.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { css } from "@emotion/css";
 
 const NavStyles = css`
-margin-bottom: 15px;
-a {
-  color: #fff;
-  padding: 6px 12px;
-  text-decoration: none;
-  border-redius: 4px;
-}
+  margin-bottom: 15px;
+  a {
+    color: #fff;
+    padding: 6px 12px;
+    text-decoration: none;
+    border-redius: 4px;
+    &.active {
+      color: #50fa7b;
+      border: 2px solid #50fa7b;
+    }
+  }
 `;
 
 const Nav = () => (
   <nav className={NavStyles}>
-    <Link to="/">Products</Link>
-    <Link to="/admin">Admin</Link>
+    <NavLink to="/" activeClassName="active" end>
+      Products
+    </NavLink>
+    <NavLink to="/admin" activeClassName="active">
+      Admin
+    </NavLink>
   </nav>
 );
 

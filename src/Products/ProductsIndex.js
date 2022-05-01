37e1a5e88f1dc8b22406e87987ccd67056d23de9
aljagne <ajagne01@gmail.com>
2022-05-01 10:59:56 +0000

    Products components created✅

--- src/Products/ProductsIndex.js
+++ src/Products/ProductsIndex.js
@@ -1,9 +1,25 @@
-import React from  "react";
+import React, { useState, useEffect } from "react";
+
+import { listProducts } from "./ProductsService";
 
 const ProductsIndex = () => {
+  const [products, setProducts] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const data = await listProducts();
+      setProducts(data);
+    })();
+  }, []);
+
+  if (products === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      List the products...
+      {products.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
     </div>
   );
 };

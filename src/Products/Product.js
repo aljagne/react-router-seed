02b5f8d06026a51e83022d456c4705bc1728e2ed
aljagne <ajagne01@gmail.com>
2022-05-03 10:26:25 +0000

    seeing up  with params

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
 
 const Product = () => {
+  const { id } = useParams();
+  console.log(id);
   return <div>Product</div>;
 };
 

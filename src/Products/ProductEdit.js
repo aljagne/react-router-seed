18eab13e97255e30e80428723a475a0abc86c0f4
aljagne <ajagne01@gmail.com>
2022-05-04 14:11:26 +0000

    Redirecting upon product create✔

--- src/Products/ProductEdit.js
+++ src/Products/ProductEdit.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { css } from "@emotion/css";
 
 import { createProduct } from "./ProductsService";
@@ -8,16 +9,47 @@ const ProductEditStyles = css`
   background: #2a2c37;
   border-radius: 6px;
   padding: 15px;
+  .ProductEdit {
+    &-Input {
+      width: 100%;
+      border: 1px solid transparent;
+      color: #fff;
+      background: #1d1e26;
+      padding: 10px 15px;
+      margin-bottom: 5px;
+      border-radius: 6px;
+      outline: 0;
+      &:focus {
+        border-color: #50fa7b;
+      }
+    }
+    &-TextArea {
+      min-height: 60px;
+      resize: none;
+    }
+    &-Button {
+      border: 2px solid #50fa7b;
+      color: #50fa7b;
+      background: none;
+      padding: 10px 15px;
+      border-radius: 6px;
+      outline: 0;
+      cursor: pointer;
+      font-weight: 600;
+      text-transform: uppercase;
+    }
+  }
 `;
 
 const ProductEdit = () => {
+  const navigate = useNavigate();
   const [form, setForm] = useState(null);
 
   useEffect(() => {
     setForm({
       id: "",
       name: "",
-      price: "",
+      price: 0,
       description: "",
     });
   }, []);
@@ -29,18 +61,18 @@ const ProductEdit = () => {
     });
   };
 
-  if (form === null) {
-    return <div>Loading...</div>;
-  }
-
   const handleCreate = async () => {
     try {
-      const created = await createProduct(form);
-      console.log(created);
+      const { id } = await createProduct(form);
+      navigate(`/admin/${id}`);
     } catch (e) {
       console.warn(e);
     }
   };
+
+  if (form === null) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <form className={ProductEditStyles}>
@@ -78,7 +110,7 @@ const ProductEdit = () => {
         onChange={({ target }) => updateField(target)}
       />
       <button
-        type="submit"
+        type="button"
         className="ProductEdit-Button"
         onClick={handleCreate}
       >
